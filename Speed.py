3910c5719d1bb6d8d068b788c27aa4d2ff5f0e6e
Gaurav <48359237+Ghack9@users.noreply.github.com>
2020-08-22 09:03:49 +0530

    Main File
    Use #"Pip install " to install modules in your system

---
+++ Speed.py
@@ -0,0 +1,26 @@
+import speedtest
+from tabulate import tabulate
+
+st=speedtest.Speedtest()
+
+class Network(object):
+    def __init__(self):
+        self.parser = speedtest.Speedtest()
+
+    def data(self):
+        down = str(f"{round(self.parser.download() / 1_000_000 , 2)} Mbps")
+        up = str(f"{round(self.parser.upload() / 1_000_000, 2)} Mbps")
+        servernames =[]   
+        st.get_servers(servernames)   
+        png=st.results.ping
+
+        return [["Interface" , "Download", "Upload","Ping"],["JioFi2_06CE6B",down,up,png]]
+
+
+
+    def __repr__(self):
+        speed = self.data()
+        return tabulate(speed, headers="firstrow", tablefmt="fancy_grid")
+
+if __name__ == "__main__":
+    print(Network())
